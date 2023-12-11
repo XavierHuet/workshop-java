3a8f3ecf209c52ed803dd5b012f4f28defdd7875
Xavier <xavier.huet@epitech.eu>
2023-12-11 10:58:37 +0400

    1st task

---
+++ JavaProgram.java
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** workshop-java
+** File description:
+** JavaProgeam
+*/
+
+public class JavaProgram {
+    public static void main(String []args) {
+        Vehicle car1 = new Vehicle("Volkswagen", "Scirocco", 2008);
+        car1.Describe();
+        car1.Accelerate();
+        }
+}
---
+++ Vehicle.java
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2023
+** workshop-java
+** File description:
+** vehicle
+*/
+
+public class Vehicle {
+    String Brand;
+    String Model;
+    int Year;
+    int Weight;
+    int Wheels;
+
+    public Vehicle(String brand, String model, int year) {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    public void Accelerate() {
+        System.out.println("I'm putting the pedal to the metal");
+    }
+
+    public void Brake() {
+        System.out.println("Woah! Slow down boy!");
+    }
+
+    public void Describe() {
+        System.out.println("I'm " + Brand + " " + Model + " " + "made in " + Year);
+    }
+}
